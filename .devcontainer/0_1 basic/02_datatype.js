44afb84749f9314bb13b02190005e64a621514b9
gurpreetsingh102 <gurpreetsinghpb1088@gmail.com>
2026-05-27 00:20:02 +0000

    conversion of data type

---
+++ .devcontainer/0_1 basic/02_datatype.js
@@ -0,0 +1,26 @@
+let score = "33"; // string
+
+let valueInNumber = Number(score);  // convert string to number
+
+
+console.log(typeof score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);        
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase => 0;
+
+let isLoggedIn = "gurpreet" // string
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)   // convert string to boolean
+console.log(booleanIsLoggedIn);  //print the result
+
+// 1 => true; 0=> false
+// "" => false
+// "gurpreet" => true
+
+let SomeNumber = "88"
+let SringSomeNumber = String(SomeNumber)  // convert number to string
+console.log(typeof SringSomeNumber);  // print the result
+console.log(SringSomeNumber);  // print the result
