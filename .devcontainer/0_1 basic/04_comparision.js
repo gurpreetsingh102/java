cafe52d3ab151868b7796ce5fb5278dc51391ecb
gurpreetsingh102 <gurpreetsinghpb1088@gmail.com>
2026-05-27 11:58:32 +0000

    comparision config done

--- .devcontainer/0_1 basic/04_comparision.js
+++ .devcontainer/0_1 basic/04_comparision.js
@@ -1,4 +1,25 @@
-console.log(2 > 1)
+    //Comparision operation     AND     Equality operater
+
+console.log(288 > 555)
 console.log(2 < 1)
 console.log(2 >= 1)
 console.log(2 != 1)
+
+
+//      Equality operater
+
+console.log("8" == 0) 
+// Loose equality operator
+// Used for checking both values are equal or not
+
+console.log(6 === 6) 
+// Strict equality operator
+// Used for checking value and datatype both are same
+
+console.log(6 != 5)
+// Loose not equal operator
+// Used for checking both values are different or not
+
+console.log(0 !== 9) 
+// Strict not equal operator
+// Used for checking values or datatype are not same
