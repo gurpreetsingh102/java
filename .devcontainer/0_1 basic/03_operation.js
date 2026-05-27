e2edb5d84c1c01da8ba5d2d8ec1270d036abb2c9
gurpreetsingh102 <gurpreetsinghpb1088@gmail.com>
2026-05-27 10:47:43 +0000

    config done

---
+++ .devcontainer/0_1 basic/03_operation.js
@@ -0,0 +1,28 @@
+let y = 3 // is my value
+let negy = -y // Convert into negative value
+console.log(negy) //print the Result
+
+console.log(2 + 8)
+console.log(2 * 8)
+console.log(2 / 8)
+console.log(2 % 8)
+console.log(2 + "5" + 8)
+
+console.log("6" * 5 + 9)
+console.log(6 * 5 + "9")
+
+
+console.log(-true); //boolean value
+console.log(+""); // + value string nu number ch convert karda ha
+
+// They are two type like Prefix and postfix 
+ 
+
+let gamecounter = 100
+gamecounter ++; // This is post increment means add the one number in value
+console.log(gamecounter);
+
+
+let x = 100
+--x; //this is prefix Decrement Method Means dicrease the one value from orignal value
+console.log(x);
