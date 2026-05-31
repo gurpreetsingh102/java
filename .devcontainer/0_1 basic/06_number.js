0050458acdbb07420dcfaafdf15760922898f923
gurpreetsingh102 <gurpreetsinghpb1088@gmail.com>
2026-05-31 10:30:36 +0000

    Number and Math Config done

---
+++ .devcontainer/0_1 basic/06_number.js
@@ -0,0 +1,44 @@
+const score =500
+// console.log(score)
+
+const balance = 1000
+// console.log(balance)
+
+// console.log(balance.toString().length); // how many character are inside 
+// console.log(balance.toFixed(1)); // give value in .0 number
+
+const otherNumber = 23.5648
+// console.log(otherNumber.toPrecision(3));
+
+
+const value = 10000000
+// console.log(value.toLocaleString()) // in us vale
+// console.log(value.toLocaleString('en-IN')) // indian number
+
+// console.log(Math); 
+// console.log(Math.abs(4));  // same number
+// console.log(Math.round(4.2)); 
+// console.log(Math.ceil(4.2)); // upper Number
+// console.log(Math.floor(4.8)); // small number
+// console.log(Math.min(4, 8, 9 , 2 , 3 , 0));  // giver minmum value
+// console.log(Math.max(4, 8, 9 , 2 , 3 , 0));  // giver upper whole value
+
+// console.log(Math.random()); // give any random value in point(.)
+//  console.log(Math.random()*10); 
+// console.log((Math.random()*10) + 1); 
+
+
+// console.log((Math.floor(Math.random()*10) + 1));  // give only one value not in . value give
+
+
+ const num1 = 10
+ const num2 = 20
+
+console.log(Math.floor(Math.random() * (num2 - num1 + 1))); // give value under 0 to 10 
+ // Math.random give any value
+ // Math.floor Remove decimal value
+
+//  Math.random()      // 0.73
+// 0.73 * 11          // 8.03
+// Math.floor(8.03)   // 8
+// console.log(8)     // prints 8
